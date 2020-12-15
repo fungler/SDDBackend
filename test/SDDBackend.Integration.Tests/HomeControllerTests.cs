87d70b74497a76e6f6a4579833e442548e061535
garb0nzo <54480601+weetardeded@users.noreply.github.com>
2020-12-15 10:49:51 +0100

    simple integration test

---
+++ test/SDDBackend.Integration.Tests/HomeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SDDBackend.Integration.Tests
+{
+
+    public class HomeControllerTests : WebApplicationFactory<Startup>
+    {
+        private readonly HttpClient client;
+
+        public HomeControllerTests()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            client = appFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetHttpRequest()
+        {
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7001/api/home/registerJson/getState?name=inst-sim-01");
+            HttpStatusCode status = response.StatusCode;
+
+            // Assert 
+            Assert.Equal<HttpStatusCode>(HttpStatusCode.OK, status);
+        }
+
+    }
+}
