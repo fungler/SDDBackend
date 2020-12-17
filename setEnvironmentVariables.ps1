66743f26e92c336bf17b87b5630a32e5e24042d6
Annika Ehlers <e_hlers@live.dk>
2020-12-17 11:37:36 +0100

    Test env

--- setEnvironmentVariables.ps1
+++ setEnvironmentVariables.ps1
@@ -1,7 +1,7 @@
 Param(
-[string]$SCD_Access
+[string]$scd
 )
-Write-Output "sauce.userName value from ADO was passed as an Argument in the ADO Task called `$env:SCD_Access`
-to sauceUserName variable in the Posh. This is the value found=>$SCD_Access"
+Write-Output "sauce.userName value from ADO was passed as an Argument in the ADO Task called `$env:GitHubPAT`
+to sauceUserName variable in the Posh. This is the value found=>$GitHubPAT"
 
-[Environment]::SetEnvironmentVariable("SCD_Access", "$SCD_Access", "User")
+[Environment]::SetEnvironmentVariable("GitHubPAT", "$scd", "User")
